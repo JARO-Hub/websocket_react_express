3a60a259ab4eba38b6a489e28f4fa1f96e596deb
tempra <dylantempra1@gmail.com>
2025-11-22 23:05:23 -0400

    feat: tareas completadas  (HU5-T4/T5)

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+// frontend/src/main.tsx
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// 1. Importar el Router y el AuthProvider
+import AppRouter from './router/AppRouter'; 
+import { AuthProvider } from './context/AuthContext'; 
+import './index.css'; // Mantenemos la importación de CSS si existe
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    {/* 2. Envolver toda la aplicación con el AuthProvider (HU5-T4) */}
+    <AuthProvider>
+      {/* 3. Renderizar el enrutador principal */}
+      <AppRouter />
+    </AuthProvider>
   </React.StrictMode>,
-)
+);
